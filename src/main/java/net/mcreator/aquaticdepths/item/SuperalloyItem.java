bbaa08b52174e363c37cefc0e3be0381baab9612
Dem0x <Yaroslav@DESKTOP-HS8TA5I>
2024-02-16 13:02:32 +0200

    Commit3 - added SuperAlloy

---
+++ src/main/java/net/mcreator/aquaticdepths/item/SuperalloyItem.java
@@ -0,0 +1,64 @@
+
+package net.mcreator.aquaticdepths.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.aquaticdepths.itemgroup.AquaticDepthsItemGroup;
+import net.mcreator.aquaticdepths.AquaticDepthsModElements;
+
+import java.util.List;
+
+@AquaticDepthsModElements.ModElement.Tag
+public class SuperalloyItem extends AquaticDepthsModElements.ModElement {
+	@ObjectHolder("aquatic_depths:superalloy")
+	public static final Item block = null;
+
+	public SuperalloyItem(AquaticDepthsModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(AquaticDepthsItemGroup.tab).maxStackSize(64).isImmuneToFire().rarity(Rarity.EPIC));
+			setRegistryName("superalloy");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Such storng material.Its power and durability bend space"));
+		}
+	}
+}
